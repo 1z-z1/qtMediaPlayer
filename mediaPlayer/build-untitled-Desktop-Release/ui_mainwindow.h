4b642aeb66a7a8adcc5d0ba65e36f5c164157bcf
enigmatic-system <admin@enigmaticsystem.xyz>
2020-02-09 18:24:15 -0800

    Inital commit

---
+++ mediaPlayer/build-untitled-Desktop-Release/ui_mainwindow.h
@@ -0,0 +1,122 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.14.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QAction *actionOpen;
+    QAction *actionPlay;
+    QAction *actionPause;
+    QAction *actionStop;
+    QWidget *centralwidget;
+    QMenuBar *menubar;
+    QMenu *menuFile;
+    QMenu *menuVideo;
+    QStatusBar *statusbar;
+    QToolBar *toolBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(647, 431);
+        MainWindow->setStyleSheet(QString::fromUtf8("background:rgb(51, 51, 51);"));
+        actionOpen = new QAction(MainWindow);
+        actionOpen->setObjectName(QString::fromUtf8("actionOpen"));
+        QIcon icon;
+        icon.addFile(QString::fromUtf8(":/images/icons/list.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionOpen->setIcon(icon);
+        actionPlay = new QAction(MainWindow);
+        actionPlay->setObjectName(QString::fromUtf8("actionPlay"));
+        QIcon icon1;
+        icon1.addFile(QString::fromUtf8(":/images/icons/play-button.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionPlay->setIcon(icon1);
+        actionPause = new QAction(MainWindow);
+        actionPause->setObjectName(QString::fromUtf8("actionPause"));
+        QIcon icon2;
+        icon2.addFile(QString::fromUtf8(":/images/icons/pause.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionPause->setIcon(icon2);
+        actionStop = new QAction(MainWindow);
+        actionStop->setObjectName(QString::fromUtf8("actionStop"));
+        QIcon icon3;
+        icon3.addFile(QString::fromUtf8(":/images/icons/stop.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionStop->setIcon(icon3);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 647, 15));
+        menuFile = new QMenu(menubar);
+        menuFile->setObjectName(QString::fromUtf8("menuFile"));
+        menuVideo = new QMenu(menubar);
+        menuVideo->setObjectName(QString::fromUtf8("menuVideo"));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+        toolBar = new QToolBar(MainWindow);
+        toolBar->setObjectName(QString::fromUtf8("toolBar"));
+        MainWindow->addToolBar(Qt::BottomToolBarArea, toolBar);
+
+        menubar->addAction(menuFile->menuAction());
+        menubar->addAction(menuVideo->menuAction());
+        menuFile->addAction(actionOpen);
+        menuVideo->addAction(actionPlay);
+        menuVideo->addAction(actionPause);
+        menuVideo->addAction(actionStop);
+        toolBar->addAction(actionOpen);
+        toolBar->addSeparator();
+        toolBar->addAction(actionPlay);
+        toolBar->addAction(actionPause);
+        toolBar->addAction(actionStop);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        actionOpen->setText(QCoreApplication::translate("MainWindow", "Open", nullptr));
+#if QT_CONFIG(tooltip)
+        actionOpen->setToolTip(QCoreApplication::translate("MainWindow", "Open a file", nullptr));
+#endif // QT_CONFIG(tooltip)
+        actionPlay->setText(QCoreApplication::translate("MainWindow", "Play", nullptr));
+        actionPause->setText(QCoreApplication::translate("MainWindow", "Pause", nullptr));
+        actionStop->setText(QCoreApplication::translate("MainWindow", "Stop", nullptr));
+        menuFile->setTitle(QCoreApplication::translate("MainWindow", "File", nullptr));
+        menuVideo->setTitle(QCoreApplication::translate("MainWindow", "Video", nullptr));
+        toolBar->setWindowTitle(QCoreApplication::translate("MainWindow", "toolBar", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
